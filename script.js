832bcdc6c46fa856d8b3c7b61cb4d89516fe6f9c
souradeep Guha Neogi <39113709+sourad80@users.noreply.github.com>
2020-07-01 12:14:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 function logfrm(){
-    var x2 = document.getElementById("fname").value;
+    var x2 = document.forms["login"]["fname"].value;
     var atposition=x2.indexOf("@");  
     var dotposition=x2.lastIndexOf(".");  
+    var x1 = document.forms["login"]["fpass"].value;
+    var passLen = 8;
     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-        alert("Please enter a valid e-mail address");  
-        return false
+        alert("Please enter a valid e-mail address!!!!");  
+        return false;
     }  
-    else{
-        var x1 = document.getElementById("fpass").value;
-        alert(x1.length);
-        if(x1.length < 8)
-        {
-            alert("Password should be more than 8 character!!!!");
-            return false;
-        }
+    if(x1.length < passLen)
+    {
+        alert("Password should be more than 8 character!!!!");
+        return false;
+    }
 
-        var x = document.getElementById("frem");
-        if(x.checked == true)
-        {
-            document.getElementById("fname").value = x2;
-            document.getElementById("fpass").value = x1;
-            // document.forms["login"]["fname"].value = x2
-            // document.forms["login"]["fpass"].value = x1
-        }
-        else{
-            document.getElementById("fname").value = "";
-            document.getElementById("fpass").value = "";
-        }
+    var x = document.forms["login"]["frem"];
+    if(x.checked == true)
+    {
+        // document.getElementById("fname").value = x2;
+        // document.getElementById("fpass").value = x1;
+        document.forms["login"]["fname"].value = x2
+        document.forms["login"]["fpass"].value = x1
+        return false;
+    }
+    else{
+        document.getElementById("fname").value = "";
+        document.getElementById("fpass").value = "";
     }
 } 
