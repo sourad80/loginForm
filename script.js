1e1edb84c98297a5021e7f61819fcd73d8239bd2
souradeep Guha Neogi <39113709+sourad80@users.noreply.github.com>
2020-07-01 13:35:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 function logfrm(){
+    
     var x2 = document.forms["login"]["fname"].value;
     var atposition=x2.indexOf("@");  
     var dotposition=x2.lastIndexOf(".");  
-    var x1 = document.forms["login"]["fpass"].value;
+    
+    var x1 = document.forms["login"]["fpass"];
     var passLen = 8;
-    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+
+    // console.log(x1.value)
+
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x2.length){  
         alert("Please enter a valid e-mail address!!!!");  
         return false;
     }  
-    if(x1.length < passLen)
+
+    if(x1.value.length < passLen)
     {
         alert("Password should be more than 8 character!!!!");
         return false;
     }
+    
+    console.log("No error upto here");
 
-    var x = document.forms["login"]["frem"];
-    if(x.checked == true)
+    var x = document.getElementById("frem").checked;
+    
+    console.log(x)
+
+    if(x == true)
     {
         // document.getElementById("fname").value = x2;
         // document.getElementById("fpass").value = x1;
-        document.forms["login"]["fname"].value = x2
-        document.forms["login"]["fpass"].value = x1
+        document.getElementById("fname").value = x2
+        document.getElementById("fpass").value = x1.value
         return false;
     }
     else{
